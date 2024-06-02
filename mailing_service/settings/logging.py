93c689dace572ee2bf4040522b51e5ce1eefb6a2
m-dzn <mdzn.dev@gmail.com>
2024-06-02 11:36:50 +0900

    chore: adjust DJANGO_LOG_LEVEL env

--- mailing_service/settings/logging.py
+++ mailing_service/settings/logging.py
@@ -1,30 +1,17 @@
+import os
 
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {message}',
-            'style': '{',
-        },
-    },
     'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'django_log.log',
-            'formatter': 'verbose',
-        }
-    },
-    'root': {
-        'handlers': ['file'],
-        'level': 'DEBUG',
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
     },
     'loggers': {
         'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
         },
     },
 }
